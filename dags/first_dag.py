1a44367c30b3b2bf2007dda486626adaa8d84637
Muhammadqodir2005 <muhammadqodir2>
2026-09-02 08:58:45 +0500

    example dags are hidden and started working on first dag

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -1,3 +1,22 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
 
+
+def hello():
+    print("Hello from my first Airflow DAG!")
+
+
+with DAG(
+    dag_id="first_dag",
+    start_date=datetime(2026, 9, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    task = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello,
+    )
 
 
