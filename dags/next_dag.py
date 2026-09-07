149e9da7b5f09d16d53e47b7efaab1f0ecfeed43
Muhammadqodir2005 <muhammadqodir2>
2026-09-07 09:23:16 +0500

    new changes are made

--- dags/next_dag.py
+++ dags/next_dag.py
@@ -3,6 +3,12 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+
+def hello_world():
+    return 'Hello World!'
+
+
+
 with DAG(
     dag_id = 'next_dag',
     description = 'next dag has been created',
@@ -12,12 +18,17 @@ with DAG(
 ) as dag:
     task1 = PythonOperator(
         task_id='task1',
+        python_callable=hello_world,
     )
     task2 = PythonOperator(
         task_id='task2',
+        python_callable=hello_world,
+
     )
     task3 = PythonOperator(
         task_id='task3',
+        python_callable=hello_world,
+
     )
 
     task1 >> task2>>task3
